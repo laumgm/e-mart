00f625e32fe20570826115fb5ec99157bbd6c731
laumgm <lauramarie.manlapaz@tup.edu.ph>
2021-06-14 13:26:06 +0800

    searchbox productlist seller route

--- frontend/src/components/Paginate.js
+++ frontend/src/components/Paginate.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Pagination } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 
-const Paginate = ({ pages, page, isAdmin = false, keyword = '', category = '' }) => {
+const Paginate = ({ pages, page, isAdmin=false, isSeller=false, keyword = '', category = '' }) => {
+    useEffect(() => {
+        console.log('paginate isAdmin', isAdmin)
+        console.log('paginate isSeller', isSeller)
+    })
     return pages > 1 && (
         <Pagination>
             {[...Array(pages).keys()].map(x => (
                 <LinkContainer 
                   key={x + 1} 
-                  to={!isAdmin ? 
-                    keyword ? `/search/${keyword}/page/${x+1}` 
+                  to={isAdmin ?`/admin/productlist/${x+1}` 
+                    : isSeller ? `/productlist/seller/${x+1}`
+                    : keyword ? `/search/${keyword}/page/${x+1}` 
                     : category ? `/category/${category}/page/${x+1}`
-                    : `/page/${x+1}` 
-                    : `/admin/productList/${x+1}`
+                    : `/page/${x+1}`
                   }
                 >
                     <Pagination.Item active={x+1 === page}>
