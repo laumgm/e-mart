3b15ab9df0e6efcee43c9be7206b171978ffb5d2
laumgm <lauramarie.manlapaz@tup.edu.ph>
2021-06-14 17:24:35 +0800

    delete comment

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -6,16 +6,14 @@ export default function Product( {product} ) {
  //const {product} = props; 
   return (
     <Card className='my-3 p-3 rounded'>
-      {/* <Container className=''> */}
         <Link to={`/product/${product._id}`}>
           <Card.Img src={product.image} variant='top' alt={product.name}/>
         </Link>
-      {/* </Container> */}
       <Card.Body>
         <Link to={`/product/${product._id}`}>
           <Card.Title as='div'>
             <strong>
-              {product.name.length > 30 ? product.name.substring(0, 30) + '...' : product.name}
+              {product.name.length > 28 ? product.name.substring(0, 28) + '...' : product.name}
             </strong>
           </Card.Title>
         </Link>
